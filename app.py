8e9c4b9da11dbb1b1750ec3e9871ba14ae2727ca
Chacha_now <158858982+foxdan1779@users.noreply.github.com>
2026-08-14 17:49:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ except ImportError:
 CONFIG = {
     'output_dir': './videos',
     'temp_dir': './temp',
-    'replicate_token': 'r8_GMDrt31b4LSlkPjJ64AD9w1tgjaBSQL0G9Tol',  # 👈 ВСТАВЬТЕ СВОЙ ТОКЕН
+    'replicate_token': 'r8_HHIJKMhVMN0vqXrSLQpWLMvN3ssqdHV1X7Hx0',  # 👈 ВСТАВЬТЕ СВОЙ ТОКЕН
     'replicate_model': 'stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf'
 }
 
